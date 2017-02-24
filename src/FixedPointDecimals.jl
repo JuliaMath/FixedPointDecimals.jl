31251fd72250afb91d29c4fd45b28f503d6131e9
Fengyang Wang <fengyang.wang.0@gmail.com>
2017-02-24 15:21:12 -0500

    Introduce FixedPointDecimals (#69)
    Introduce FixedPointDecimals and have Monetary use it

--- src/FixedPointDecimals.jl
+++ src/FixedPointDecimals.jl
@@ -1,5 +1,227 @@
+# This file partially based off JuliaMath/FixedPointNumbers.jl
+#
+# Copyright (c) 2014: Jeff Bezanson and other contributors.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 module FixedPointDecimals
 
-# package code goes here
+export FixedDecimal
 
-end # module
+using Compat
+
+import Base: reinterpret, zero, one, abs, sign, ==, <, <=, +, -, /, *, div,
+             rem, divrem, fld, mod, fldmod, fld1, mod1, fldmod1, isinteger,
+             typemin, typemax, realmin, realmax, show, convert, promote_rule,
+             min, max, trunc, round, floor, ceil, eps, float
+
+"""
+    FixedDecimal{I <: Integer, f::Int}
+
+A fixed-point decimal type backed by integral type `I`, with `f` digits after
+the decimal point stored.
+"""
+immutable FixedDecimal{T <: Integer, f} <: Real
+    i::T
+
+    # internal constructor
+    Base.reinterpret{T, f}(::Type{FixedDecimal{T, f}}, i::Integer) =
+        new{T, f}(i % T)
+end
+
+const FD = FixedDecimal
+
+floattype{T<:Union{Int8, UInt8, Int16, UInt16}, f}(::Type{FD{T, f}}) = Float32
+floattype{T<:Integer, f}(::Type{FD{T, f}}) = Float64
+floattype{f}(::Type{FD{BigInt, f}}) = BigFloat
+
+# basic operators
+-{T, f}(x::FD{T, f}) = reinterpret(FD{T, f}, -x.i)
+abs{T, f}(x::FD{T, f}) = reinterpret(FD{T, f}, abs(x.i))
+
++{T, f}(x::FD{T, f}, y::FD{T, f}) = reinterpret(FD{T, f}, x.i+y.i)
+-{T, f}(x::FD{T, f}, y::FD{T, f}) = reinterpret(FD{T, f}, x.i-y.i)
+
+function _round_to_even(quotient, remainder, powt)
+    if powt == 1
+        quotient
+    else
+        halfpowt = powt >> 1
+        if remainder == halfpowt
+            ifelse(iseven(quotient), quotient, quotient + one(quotient))
+        elseif remainder < halfpowt
+            quotient
+        else
+            quotient + one(quotient)
+        end
+    end
+end
+
+# multiplication rounds to nearest even representation
+# TODO: can we use floating point to speed this up? after we build a
+# correctness test suite.
+function *{T, f}(x::FD{T, f}, y::FD{T, f})
+    powt = T(10)^f
+    quotient, remainder = fldmod(Base.widemul(x.i, y.i), powt)
+    reinterpret(FD{T, f}, _round_to_even(quotient, remainder, powt))
+end
+
+# these functions are needed to avoid InexactError when converting from the
+# integer type
+*{T, f}(x::Integer, y::FD{T, f}) = reinterpret(FD{T, f}, T(x * y.i))
+*{T, f}(x::FD{T, f}, y::Integer) = reinterpret(FD{T, f}, T(x.i * y))
+
+# TODO. this is probably wrong sometimes.
+function /{T, f}(x::FD{T, f}, y::FD{T, f})
+    powt = T(10)^f
+    quotient, remainder = divrem(x.i, y.i)
+    reinterpret(FD{T, f}, quotient * powt + round(T, remainder / y.i * powt))
+end
+
+# integerification
+trunc{T, f}(x::FD{T, f}) = FD{T, f}(div(x.i, T(10)^f))
+floor{T, f}(x::FD{T, f}) = FD{T, f}(fld(x.i, T(10)^f))
+# TODO: round with number of digits; should be easy
+function round{T, f}(x::FD{T, f})
+    powt = T(10)^f
+    quotient, remainder = fldmod(x.i, powt)
+    FD{T, f}(_round_to_even(quotient, remainder, powt))
+end
+function ceil{T, f}(x::FD{T, f})
+    powt = T(10)^f
+    quotient, remainder = fldmod(x.i, powt)
+    if remainder > 0
+        FD{T, f}(quotient + one(quotient))
+    else
+        FD{T, f}(quotient)
+    end
+end
+
+for truncfn in [:trunc, :round, :floor, :ceil]
+    @eval $truncfn{TI}(::Type{TI}, x::FD)::TI = $truncfn(x)
+end
+
+# conversions and promotions
+convert{T, f}(::Type{FD{T, f}}, x::Integer) =
+    reinterpret(FD{T, f}, round(T, Base.widemul(T(x), T(10)^f)))
+
+# TODO. this is very, very incorrect.
+convert{T, f}(::Type{FD{T, f}}, x::AbstractFloat) =
+    reinterpret(FD{T, f}, round(T, T(10)^f * x))
+function convert{T, f}(::Type{FD{T, f}}, x::Rational)::FD{T, f}
+    powt = T(10)^f
+    num::T, den::T = numerator(x), denominator(x)
+    g = gcd(powt, den)
+    powt = div(powt, g)
+    den = div(den, g)
+    reinterpret(FD{T, f}, powt * num) / FD{T, f}(den)
+end
+
+function convert{T, f, U, g}(::Type{FD{T, f}}, x::FD{U, g})
+    if f ≥ g
+        reinterpret(FD{T, f}, convert(T, Base.widemul(T(10)^(f-g), x.i)))
+    else
+        sf = T(10)^(g - f)
+        q, r = divrem(x.i, sf)
+        if r ≠ 0
+            throw(InexactError())
+        else
+            reinterpret(FD{T, f}, convert(T, q))
+        end
+    end
+end
+
+for remfn in [:rem, :mod, :mod1, :min, :max]
+    @eval $remfn{T <: FD}(x::T, y::T) = reinterpret(T, $remfn(x.i, y.i))
+end
+for divfn in [:div, :fld, :fld1]
+    @eval $divfn{T <: FD}(x::T, y::T) = $divfn(x.i, y.i)
+end
+
+convert(::Type{AbstractFloat}, x::FD) = convert(floattype(typeof(x)), x)
+convert{TF <: AbstractFloat, T, f}(::Type{TF}, x::FD{T, f})::TF =
+    x.i / TF(10)^f
+
+function convert{TI <: Integer, T, f}(::Type{TI}, x::FD{T, f})::TI
+    isinteger(x) || throw(InexactError())
+    div(x.i, T(10)^f)
+end
+
+convert{TR<:Rational,T,f}(::Type{TR}, x::FD{T, f})::TR =
+    x.i // T(10)^f
+
+promote_rule{T, f, TI <: Integer}(::Type{FD{T, f}}, ::Type{TI}) = FD{T, f}
+promote_rule{T, f, TF <: AbstractFloat}(::Type{FD{T, f}}, ::Type{TF}) = TF
+promote_rule{T, f, TR}(::Type{FD{T, f}}, ::Type{Rational{TR}}) = Rational{TR}
+
+# comparison
+=={T <: FD}(x::T, y::T) = x.i == y.i
+ <{T <: FD}(x::T, y::T) = x.i  < y.i
+<={T <: FD}(x::T, y::T) = x.i <= y.i
+
+# predicates and traits
+isinteger{T, f}(x::FD{T, f}) = rem(x.i, T(10)^f) == 0
+typemin{T, f}(::Type{FD{T, f}}) = reinterpret(FD{T, f}, typemin(T))
+typemax{T, f}(::Type{FD{T, f}}) = reinterpret(FD{T, f}, typemax(T))
+eps{T <: FD}(::Type{T}) = reinterpret(T, 1)
+eps(x::FD) = eps(typeof(x))
+realmin{T <: FD}(::Type{T}) = eps(T)
+realmax{T <: FD}(::Type{T}) = typemax(T)
+
+# printing
+function show{T}(io::IO, x::FD{T, 0})
+    iscompact = get(io, :compact, false)
+    if !iscompact
+        print(io, "FixedDecimal{$T,0}(")
+    end
+    print(io, x.i)
+    if !iscompact
+        print(io, ')')
+    end
+end
+function show{T, f}(io::IO, x::FD{T, f})
+    iscompact = get(io, :compact, false)
+
+    # note: a is negative if x.i == typemin(x.i)
+    s, a = sign(x.i), abs(x.i)
+    integer, fractional = divrem(a, T(10)^f)
+    integer = abs(integer)  # ...but since f > 0, this is positive
+    fractional = abs(fractional)
+
+    if !iscompact
+        print(io, "FixedDecimal{$T,$f}(")
+    end
+    if s == -1
+        print(io, "-")
+    end
+    fractionchars = lpad(abs(fractional), f, "0")
+    if iscompact
+        fractionchars = rstrip(fractionchars, '0')
+        if isempty(fractionchars)
+            fractionchars = "0"
+        end
+    end
+    print(io, integer, '.', fractionchars)
+    if !iscompact
+        print(io, ')')
+    end
+end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,226 @@
 using FixedPointDecimals
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+const SFD2 = FixedDecimal{Int16, 2}
+const SFD4 = FixedDecimal{Int16, 4}
+const FD2 = FixedDecimal{Int, 2}
+const FD4 = FixedDecimal{Int, 4}
+const WFD2 = FixedDecimal{Int128, 2}
+const WFD4 = FixedDecimal{Int128, 4}
+
+const keyvalues = [typemin(FD2),
+                   FD2(-0.01),
+                   FD2(0),
+                   FD2(0.01),
+                   FD2(1),
+                   typemax(FD2)]
+
+@testset "conversion" begin
+    @testset for x in keyvalues
+        @testset for T in [Rational{Int128}, WFD2, WFD4]
+            @test convert(FD2, convert(T, x)) == x
+        end
+        if 0 ≤ abs(x) < 2
+            @testset for T in [SFD2, SFD4, FD4]
+                @test convert(FD2, convert(T, x)) == x
+            end
+        end
+    end
+
+    @test_throws InexactError convert(FD2, FD4(0.0001))
+    @test_throws InexactError convert(FD4, typemax(FD2))
+    @test_throws InexactError convert(SFD2, typemax(FD2))
+end
+
+@testset "promotion" begin
+    @test 1//10 + FD2(0.1) === 1//5
+    @test 0.1 + FD2(0.1) === 0.2
+    @test 1 + FD2(0.1) === FD2(1.1)
+end
+
+@testset "float" begin
+    @test float(-one(SFD2)) === -1.0f0
+    @test float(zero(SFD2)) === 0.0f0
+    @test float(one(SFD2)) === 1.0f0
+    @test float(-one(FD2)) === -1.0
+    @test float(zero(FD2)) === 0.0
+    @test float(one(FD2)) === 1.0
+end
+
+@testset "comparison" begin
+    for (i, x) in enumerate(keyvalues)
+        @test x == x
+        for y in keyvalues[i+1:end]
+            @test x ≠ y
+            @test x < y
+            @test x ≤ y
+            @test y ≠ x
+            @test y > x
+            @test y ≥ x
+        end
+    end
+end
+
+@testset "zero, one" begin
+    @test FD2(0) == zero(FD2)
+    @test FD2(42.42) + FD2(0) == FD2(42.42)
+    @test FD2(1) == one(FD2)
+    @test FD2(42.42) * FD2(1) == FD2(42.42)
+end
+
+@testset "eps, realmin, realmax" begin
+    @test realmin(FD2) == eps(FD2) == FD2(0.01)
+    @test eps(FD2(1.11)) == FD2(0.01)
+    for x in keyvalues
+        if x ≠ typemax(FD2)
+            @test x + eps(x) > x
+        end
+        if x ≠ typemin(FD2)
+            @test x - eps(x) < x
+            if x ≠ 0
+                @test realmin(FD2) ≤ abs(x) ≤ realmax(FD2)
+            end
+        end
+    end
+end
+
+@testset "addition" begin
+    @test FD2(0) + FD2(0) == FD2(0)
+    @test FD2(1.11) + FD2(2.22) == FD2(3.33)
+    @test FD2(0.01) + FD2(0.01) == FD2(0.02)
+    @test FD2(0.01) + FD2(-0.01) == FD2(0)
+
+    # overflow
+    @test typemax(FD2) + eps(FD2) == typemin(FD2)
+end
+
+@testset "subtraction" begin
+    for x in keyvalues
+        @test x - x == 0
+        for y in keyvalues
+            @test x + y - y == x
+            @test y + x - y == x
+        end
+    end
+end
+
+@testset "multiplication" begin
+    @testset "with integer" begin
+        for x in keyvalues
+            @test 1 * x == x * 1 == x
+            @test one(x) * x == x * one(x) == x
+            @test (-1) * x == x * (-1) == -x
+            @test 2 * x == x + x == (one(x) + one(x)) * x
+        end
+    end
+
+    @testset "binary" begin
+        @test FD2(0.33) * FD2(1.00) == FD2(0.33)
+        @test FD2(0.33) * FD2(3.00) == FD2(0.99)
+        @test FD2(0.33) * FD2(0.50) == FD2(0.16)
+        @test FD2(0.33) * FD2(0.33) == FD2(0.11)
+        @test FD2(0.67) * FD2(0.67) == FD2(0.45)
+    end
+end
+
+@testset "division by 1" begin
+    @testset for x in keyvalues
+        @test x / one(x) == x
+        @test x / -one(x) == -x
+    end
+end
+
+@testset "division by 2" begin
+    # even targets
+    for x in FD2[-0.02, 0, 0.02, 1.00]
+        for y in [2x-eps(x), 2x, 2x+eps(x)]
+            @test y / 2 == y / 2one(y) == x == y * FD2(0.5)
+        end
+    end
+
+    # odd targets
+    for x in FD2[-0.01, 0.01, 1.01]
+        y = 2x
+        @test y / 2 == y / 2one(x) == x == y * FD2(0.5)
+    end
+
+    # big numbers
+    for T in [SFD2, SFD4, FD2, FD4, WFD2, WFD4]
+        @test typemin(T) / 2 * 2 == typemin(T)
+        @test (typemax(T) / 2 - eps(T)) * 2 == typemax(T) - eps(T)
+    end
+end
+
+@testset "division by 3" begin
+    @test FD2(10) / 3 == FD2(3.33)
+    @test FD2(20) / 3 == FD2(6.67)
+    @test FD2(-1.50) / 3 == FD2(-0.50)
+    @test FD2(-20) / 3 == FD2(-6.67)
+
+    # should work with big numbers
+    @test typemin(FD2) / 3 < 0
+    @test typemax(FD2) / 3 > 0
+end
+
+@testset "abs, sign" begin
+    for x in keyvalues
+        @test sign(x)^2 ∈ [0, 1]
+        @test abs(abs(x)) == abs(x)
+        @test abs(x) * sign(x) == x
+        @test abs(x) == abs(-x)
+        if x ≠ typemin(x)
+            @test abs(x) ≥ 0
+            @test sign(x) == -sign(-x)
+        end
+        @test (abs(x) == 0) === (x == 0)
+    end
+end
+
+@testset "trunc" begin
+    @test trunc(Int, FD2(0.99)) === 0
+    @test trunc(Int, FD2(-0.99)) === 0
+    @test trunc(Int, FD2(1)) === 1
+    @test trunc(Int, FD2(-1)) === -1
+    @test trunc(typemax(FD2)) ≤ typemax(FD2)
+    @test trunc(Int, typemax(FD2)) ≤ typemax(FD2)
+    @test trunc(typemin(FD2)) ≥ typemin(FD2)
+    @test trunc(Int, typemin(FD2)) ≥ typemin(FD2)
+    @test trunc(eps(FD2)) == 0
+    @test trunc(-eps(FD2)) == 0
+end
+
+@testset "floor, ceil" begin
+    @testset for x in keyvalues
+        if 0 ≤ abs(x) < 1000
+            @test floor(x) ≤ x < floor(x) + 1
+            @test ceil(x) - 1 < x ≤ ceil(x)
+            @test isinteger(floor(x))
+            @test isinteger(ceil(x))
+        end
+    end
+end
+
+@testset "show" begin
+    @testset "compact" begin
+        @test sprint(showcompact, FD2(1.00)) == "1.0"
+        @test sprint(showcompact, FD2(1.23)) == "1.23"
+        @test sprint(showcompact, FD2(42.40)) == "42.4"
+        @test sprint(showcompact, FD2(-42.40)) == "-42.4"
+        @test sprint(showcompact, FD2(-0.01)) == "-0.01"
+        @test sprint(showcompact, FD2(0)) == "0.0"
+        for x in keyvalues
+            if 0 ≤ abs(x) < 1000
+                @test eval(parse(string(x))) == x
+            end
+        end
+
+        @test string(typemin(FixedDecimal{Int64, 2})) ==
+              "FixedDecimal{Int64,2}(-92233720368547758.08)"
+        @test string(typemax(FixedDecimal{Int64, 2})) ==
+              "FixedDecimal{Int64,2}(92233720368547758.07)"
+        @test string(typemin(FixedDecimal{Int32, 2})) ==
+              "FixedDecimal{Int32,2}(-21474836.48)"
+        @test string(typemax(FixedDecimal{Int32, 2})) ==
+              "FixedDecimal{Int32,2}(21474836.47)"
+    end
+end
