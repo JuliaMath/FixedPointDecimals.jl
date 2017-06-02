8afbabe7fb898ef3134e0e01e73eb50696f761f9
Curtis Vogt <curtis.vogt@gmail.com>
2017-06-02 11:17:08 -0500

    Switch to using `@sprintf` for Windows support

--- test/utils.jl
+++ test/utils.jl
@@ -1,26 +1,13 @@
-# Format a floating-point number as a string with a specific precision. Similar to
-# `@sprintf("%.$(dp)f", val)`.
-
-if VERSION < v"0.6-"
-    function float_string(val::AbstractFloat, dp::Integer)
-        # On Julia 0.5 positive 0.0 can have extra bits. No other integer floating-point
-        # seems to have this problem.
-        isequal(val, 0.0) && return rpad("0.", dp, '0')
-        format = "%.$(dp)f"
-        @eval @sprintf($("%.$(dp)f"), nextfloat(0.0))
-        @eval begin
-            let buffer = Array{UInt8}(100 + $dp)
-                ccall((:snprintf, :libc), Int, (Ptr{UInt8}, Csize_t, Cstring, Cdouble), buffer, length(buffer), $format, $val)
-                unsafe_string(pointer(buffer))
-            end
-        end
+# Print the floating-point number with precision high enough that rounding never occurs
+function float_string(val::AbstractFloat)
+    # Note: This function could simply be `@sprintf("%.325f", val)` once the following
+    # issue is solved: https://github.com/JuliaLang/julia/issues/22137
+    dp = 325  # Number of decimal places to print `nextfloat(0.0)` without rounding
+    int = trunc(BigInt, val)
+    if abs(int) > 0
+        dp -= ndigits(int)
     end
-else
-    function float_string(val::AbstractFloat, dp::Integer)
-        buffer = Array{UInt8}(100 + dp)
-        ccall((:snprintf, :libc), Int, (Ptr{UInt8}, Csize_t, Cstring, Cdouble), buffer, length(buffer), "%.$(dp)f", val)
-        unsafe_string(pointer(buffer))
-    end
+    @eval @sprintf($("%.$(dp)f"), $val)
 end
 
 # FixedDecimal methods which perform an alternative method of trunc, floor, and ceil which
@@ -30,9 +17,7 @@ end
 function integer_alt{T<:Integer}(::Type{T}, dp::Integer, val::AbstractFloat)
     # Note: Use a precision larger than the value can represent so that `sprintf` doesn't
     # perform any rounding.
-    # TODO: Ideally we could be using just be using `@sprintf("%.325f", val)` once this
-    # issue is fixed: https://github.com/JuliaLang/julia/issues/22137
-    str = float_string(val, 325)  # 325 digits is large enough for `nextfloat(0.0)`
+    str = float_string(val)
     sign = T(first(str) == '-' ? -1 : 1)
     decimal = findfirst(str, '.')
     int_start = sign < 0 ? 2 : 1
