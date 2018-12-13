6af05e05b3f6a7de66faf30b027d6951f72d5bb5
Nathan Daly <nhdaly@gmail.com>
2018-12-13 13:29:24 -0500

    Addressed Todd's comments

--- src/fldmod_by_const.jl
+++ src/fldmod_by_const.jl
@@ -21,12 +21,16 @@ Base.@pure function calculate_inv_coeff(::Type{T}, f) where {T}
     invcoeff = _round_to_even(fldmod(invcoeff, typemax(UT))..., typemax(UT)) % T
     return invcoeff, toshift
 end
-# These are needed to handle Int128, which widens to BigInt
-twoToTheSizeOf(::Type{T}) where {T} = typemax(widen(T))
+# These are needed to handle Int128, which widens to BigInt, since BigInt doesn't have typemax
+twoToTheSizeOf(::Type{T}) where {T} = typemax(widen(unsigned(T)))
 twoToTheSizeOf(::Union{Type{Int128}, Type{UInt128}}) = BigInt(2)^256
 twoToTheSizeOf(::Type{BigInt}) = BigInt(2)^256
 
+# This special-purpose leading_zeros is needed to handle Int128, which widens to BigInt
 _leading_zeros(x) = leading_zeros(x)
+# BigInt doesn't have a concept of "leading zeros", but since we _know_ the value being
+# passed here will fit in 256-bits (per twoToTheSizeOf), we can pretend this is a 256-bit
+# integer, take just the upper half as a UInt128, and count the leading zeros there.
 _leading_zeros(x::BigInt) = leading_zeros((x >> 128) % UInt128)
 
 
@@ -49,7 +53,7 @@ narrow(::Type{UInt16}) = UInt8
 nbits(x) = sizeof(x)*8
 
 # Implemenation based on umul32hi, from https://stackoverflow.com/a/22847373/751061
-# /* compute the upper 32 bits of the product of two unsigned 32-bit integers */
+# /* compute the upper half of the widened product of two unsigned integers */
 @inline function unsigned_splitmul_upper(a::T, b::T) where T<:Unsigned
     # /* split operands into halves */
     ah,al = splitint(a)
@@ -62,14 +66,15 @@ nbits(x) = sizeof(x)*8
     p2 = widemul(ah, bl);
     p3 = widemul(ah, bh);
     # /* sum partial products */
-    cy = ((p0 >> halfbits) + (p1%halfT) + (p2%halfT)) >> halfbits;
-    return p3 + (p2 >> halfbits) + (p1 >> halfbits) + cy;
+    carry = ((p0 >> halfbits) + (p1%halfT) + (p2%halfT)) >> halfbits;
+    return p3 + (p2 >> halfbits) + (p1 >> halfbits) + carry;
 end
 @inline function splitmul_upper(a::T, b::T) where T<:Unsigned
     return unsigned_splitmul_upper(a,b)
 end
 @inline function splitmul_upper(a::T, b::T) where T<:Signed
-    return signed(unsigned_splitmul_upper(unsigned(a), unsigned(b))) - ((a < 0) ? b : 0) - ((b < 0) ? a : 0);
+    uresult = unsigned_splitmul_upper(unsigned(a), unsigned(b))
+    return signed(uresult) - ((a < 0) ? b : 0) - ((b < 0) ? a : 0)
 end
 @inline splitmul_upper(a,b) = splitmul_upper(promote(a,b)...)
 
@@ -108,7 +113,7 @@ function div_by_const(x::T, ::Val{C}) where {T, C}
     end
     out = out >> toshift
     if T <: Signed
-        out =  out + signed     # ; leaq    (%rdx,%rax), %rax
+        out =  out + signed
     end
     return T(out)
 end
@@ -118,30 +123,34 @@ end
 
 Like `div_by_const`, but for `fld`, not `div` -- fld rounds down for negative numbers.
 """
+# This implementation was lifted directly from Base.fld(x,y)
+fld_by_const(x::T, y::Val{C}) where {T<:Unsigned, C} = div_by_const(x,y)
 function fld_by_const(x::T, y::Val{C}) where {T<:Integer, C}
     d = div_by_const(x,y)
-    return d - (signbit(x ⊻ C) & (d * C !=x))
+    return d - (signbit(x ⊻ C) & (d * C != x))
 end
 
 """
     manual_mod(x, y, quotient)
 
 Calculate `mod(x,y)` after you've already acquired quotient, the result of `fld(x,y)`.
+
+REQUIRES:
+    - `y != -1`
 """
 @inline function manual_mod(x::T, y::T, quotient::T) where T<:Integer
-    y == -1 && return T(0)   # avoid potential overflow in fld
     return x - quotient * y
 end
 
 """
-    fld_by_const(x, Val(C))
+    fldmod_by_const(x, Val(C))
 
 Returns `fldmod(x,C)`, implemented efficiently when `C` is a (positive) static Const.
 
 REQUIRES:
-  - `C` _must be_ greater than `0`
+  - `C` must be greater than `0`
 """
-function fldmod_by_const(x,y::Val{C}) where {C}
+function fldmod_by_const(x, y::Val{C}) where {C}
     d = fld_by_const(x, y)
-    d, manual_mod(promote(x, C, d)...)
+    return d, manual_mod(promote(x, C, d)...)
 end
