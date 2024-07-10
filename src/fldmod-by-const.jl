ef578e91f47ac3319b152350bf69a289a4b22823
Nathan Daly <nathan.daly@relational.ai>
2024-07-09 18:56:46 -0600

    Reorganize the functions to be top-down

--- src/fldmod-by-const.jl
+++ src/fldmod-by-const.jl
@@ -3,9 +3,8 @@
 # smaller integer sizes (<=64-bits on my 64-bit machine).
 #
 # NOTE: We use LLVM's built-in implementation for Int64 and smaller, to keep the code
-# simpler (though the code we produce is identical, verified in tests.) We apply this
-# optimization to (U)Int128 and (U)Int256, which result from multiplying FD{Int64}s and
-# FD{Int128}s.
+# simpler (though the code we produce is identical). We apply this optimization to (U)Int128
+# and (U)Int256, which result from multiplying FD{Int64}s and FD{Int128}s.
 # Before:
 # julia> @btime for _ in 1:10000 fd = fd * fd end setup = (fd = FixedDecimal{Int32,3}(1.234))
 #     54.125 μs (0 allocations: 0 bytes)   # (Unchanged)
@@ -65,6 +64,27 @@ function fld_by_const(x::T, y::Val{C}) where {T<:Signed, C}
     return d - (signbit(x ⊻ C) & (d * C != x))
 end
 
+function div_by_const(x::T, ::Val{C}) where {T, C}
+    # These checks will be compiled away during specialization.
+    # While for `*(FixedDecimal, FixedDecimal)`, C will always be a power of 10, these
+    # checks allow this function to work for any `C > 0`, in case that's useful in the
+    # future.
+    if C == 1
+        return x
+    elseif ispow2(C)
+        return div(x, C) # Will already do the right thing
+    elseif C <= 0
+        throw(DomainError("C must be > 0"))
+    end
+    # Calculate the magic number and shift amount, based on Hacker's Delight, Chapter 10.
+    magic_number, shift = magicg(typemax(T), C)
+
+    out = _widemul(promote(x, magic_number)...)
+    out >>= shift
+    # Adding one as implied by formula (1b) in Hacker's delight, Chapter 10-4.
+    return (out % T) + (x < zero(T))
+end
+
 # Unsigned magic number computation + shift by constant
 # See Hacker's delight, equations (26) and (27) from Chapter 10-9.
 # (See also the errata on https://web.archive.org/web/20190915025154/http://www.hackersdelight.org/)
@@ -104,25 +124,4 @@ Base.@assume_effects :foldable function magicg(nmax::Signed, divisor)
         end
     end
     return W(0), 0 # Should never reach here
-end
-
-function div_by_const(x::T, ::Val{C}) where {T, C}
-    # These checks will be compiled away during specialization.
-    # While for `*(FixedDecimal, FixedDecimal)`, C will always be a power of 10, these
-    # checks allow this function to work for any `C > 0`, in case that's useful in the
-    # future.
-    if C == 1
-        return x
-    elseif ispow2(C)
-        return div(x, C) # Will already do the right thing
-    elseif C <= 0
-        throw(DomainError("C must be > 0"))
-    end
-    # Calculate the magic number and shift amount, based on Hacker's Delight, Chapter 10.
-    magic_number, shift = magicg(typemax(T), C)
-
-    out = _widemul(promote(x, magic_number)...)
-    out >>= shift
-    # Adding one as implied by formula (1b) in Hacker's delight, Chapter 10-4.
-    return (out % T) + (x < zero(T))
 end
