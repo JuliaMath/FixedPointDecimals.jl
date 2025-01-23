6e763efdb8baeb42a540bdbb298eaf20312d6b7c
Bradley C. Kuszmaul <bradley.kuszmaul@relational.ai>
2025-01-23 15:28:21 +0000

    Fix up aqua test

--- test/aqua_test.jl
+++ test/aqua_test.jl
@@ -3,7 +3,6 @@
     # For some reason, running the ambiguities test as part of `test_all`
     # complains about something in BitIntegers.  But running it separately is
     # OK.
-    Aqua.test_all(FixedPointDecimals; stale_deps=false, ambiguities=false)
-    Aqua.test_stale_deps(FixedPointDecimals; ignore=[:Aqua])
+    Aqua.test_all(FixedPointDecimals; ambiguities=false)
     Aqua.test_ambiguities(FixedPointDecimals)
 end
