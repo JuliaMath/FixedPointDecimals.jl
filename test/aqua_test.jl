26e18bdb0af9a9889d0d1d5f8a027a8c94f3e0f3
Bradley C. Kuszmaul <bradley.kuszmaul@relational.ai>
2025-01-23 15:36:17 +0000

    Aqua test cleanup

--- test/aqua_test.jl
+++ test/aqua_test.jl
@@ -1,8 +1,4 @@
 @testset "Aqua" begin
     using Aqua
-    # For some reason, running the ambiguities test as part of `test_all`
-    # complains about something in BitIntegers.  But running it separately is
-    # OK.
-    Aqua.test_all(FixedPointDecimals; ambiguities=false)
-    Aqua.test_ambiguities(FixedPointDecimals)
+    Aqua.test_all(FixedPointDecimals)
 end
