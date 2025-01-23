dec2ef07bc5cf70024a2d8a6c4fba148828ddffc
Bradley C. Kuszmaul <bradley.kuszmaul@relational.ai>
2025-01-23 15:18:56 +0000

    Get the aqua tests to actually run.  Also compat with aqua 0.8

--- test/aqua_test.jl
+++ test/aqua_test.jl
@@ -1,4 +1,6 @@
-@testitem "Aqua" begin
+@testset "Aqua" begin
     using Aqua
-    Aqua.test_all(FixedPointDecimal)
+    Aqua.test_all(FixedPointDecimals; stale_deps=false, ambiguities=false)
+    Aqua.test_stale_deps(FixedPointDecimals; ignore=[:Aqua])
+    Aqua.test_ambiguities(FixedPointDecimals)
 end
