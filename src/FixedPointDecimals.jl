cdd6966ec733087e098801a4d2b1363b588ee323
Mohamad Barbar <mbarbar@users.noreply.github.com>
2025-01-29 04:38:01 +1100

    Add div_with_overflow (#106)
    * Remove promoting with_overflow functions.
    
    * Bump Project.toml: 0.5.6
    
    ---------
    
    Co-authored-by: Nathan Daly <NHDaly@gmail.com>

--- src/FixedPointDecimals.jl
+++ src/FixedPointDecimals.jl
@@ -32,7 +32,7 @@ export FixedDecimal, RoundThrows
 
 # (Re)export checked_* arithmetic functions
 # - Defined in this package:
-export checked_rdiv
+export checked_rdiv, div_with_overflow
 # - Reexported from Base:
 export checked_abs, checked_add, checked_cld, checked_div, checked_fld,
     checked_mod, checked_mul, checked_neg, checked_rem, checked_sub
@@ -440,6 +440,27 @@ end
 Base.Checked.mul_with_overflow(x::FD, y::FD) = Base.Checked.mul_with_overflow(promote(x, y)...)
 Base.Checked.mul_with_overflow(x::FD, y) = Base.Checked.mul_with_overflow(promote(x, y)...)
 Base.Checked.mul_with_overflow(x, y::FD) = Base.Checked.mul_with_overflow(promote(x, y)...)
+
+# This does not exist in Base so is just part of this package.
+# Throws on divide by zero.
+@doc """
+    div_with_overflow(x::FixedDecimal{T,f}, y::FixedDecimal{T,f})::(FixedDecimal{T,f}, Bool)
+        where {T<:Integer, f}
+
+Return the result of div (wrapping on overflow/underflow) and a boolean indicating whether
+overflow/underflow did in fact happen. Throws a DivideError on divide-by-zero.
+"""
+function div_with_overflow(x::FD{T,f}, y::FD{T,f}) where {T<:Integer,f}
+    C = coefficient(FD{T, f})
+    # This case will break the div call below.
+    if x.i == typemin(T) && y.i == -1
+        # To perform the div and overflow means reaching the max and adding 1, so typemin.
+        return (x, true)
+    end
+    # Note: The div() will throw for divide-by-zero, that's not an overflow.
+    v, b = Base.Checked.mul_with_overflow(C, div(x.i, y.i))
+    return (reinterpret(FD{T,f}, v), b)
+end
 
 Base.checked_add(x::FD, y::FD) = Base.checked_add(promote(x, y)...)
 Base.checked_sub(x::FD, y::FD) = Base.checked_sub(promote(x, y)...)
