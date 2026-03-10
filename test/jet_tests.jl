aa1f4dbf32e378b94f483b634fa0d22c0d7135a9
Drvi <tomas.drvostep@gmail.com>
2026-03-10 14:13:28 +0100

    Improve JET tests

--- test/jet_tests.jl
+++ test/jet_tests.jl
@@ -2,15 +2,17 @@ using Test
 using JET
 using FixedPointDecimals
 
-@testset "JET test_opt: FD multiplication" begin
+@testset "JET: FD multiplication" begin
     @testset for T in (Int8, Int16, Int32, Int64, Int128)
         max_f = FixedPointDecimals.max_exp10(T)
-        @testset for f in (0, 1, max_f ÷ 2, max_f)
+        @testset for f in (0:max_f)
             FD = FixedDecimal{T, f}
-            a = reinterpret(FD, T(1))
-            b = reinterpret(FD, T(2))
-            @testset let T=T, f=f
+            a = FD(1)
+            b = reinterpret(FD, typemax(T))
+            @testset let T=T, f=f, a=a, b=b
+                @test a * b == b
                 @test_opt a * b
+                @test_call a * b
             end
         end
     end
