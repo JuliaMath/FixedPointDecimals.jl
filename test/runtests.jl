39385a0499556c2f81ac02cbf4313f6bab6ef343
Nathan Daly <nathan.daly@relational.ai>
2026-03-17 08:28:26 -0600

    Drop JET changes

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,6 +11,5 @@ include(joinpath(pkg_path, "test", "utils.jl"))
 @testset "FixedPointDecimals" begin
     include("FixedDecimal.jl")
     isdefined(Base, Symbol("@assume_effects")) && include("fldmod-by-const_tests.jl")
-    include("jet_tests.jl")
     include("aqua_test.jl")
 end  # global testset
